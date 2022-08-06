c8c1460fa47d068acaa241a5f9b8e79b272bb8b0
GeorgenewtonJr <georgebandeirajunior@gmail.com>
2022-08-05 22:40:24 -0300

    inclusao do codiggo nova aplicacao ts

--- src/app/nova-aplicacao/nova-aplicacao.component.ts
+++ src/app/nova-aplicacao/nova-aplicacao.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-nova-aplicacao',
   templateUrl: './nova-aplicacao.component.html',
-  styleUrls: ['./nova-aplicacao.component.scss']
+  styleUrls: ['./nova-aplicacao.component.scss'],
 })
 export class NovaAplicacaoComponent implements OnInit {
+  form: FormGroup;
+  mode: 'create' = 'create';
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public _defaults: any,
+    private _dialogRef: MatDialogRef<NovaAplicacaoComponent>,
+    private _fb: FormBuilder
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.init();
+    this.formBuild();
   }
 
+  private formBuild() {
+    this.form = this._fb.group({
+      id: [''],
+      nome: [
+        '',
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(250),
+      ],
+      url: [
+        '',
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(250),
+      ],
+      sistemaId: [this._defaults.sistemaId, Validators.required],
+    });
+  }
+
+  init() {
+    if (this._defaults.id != null) {
+    }
+    this.loadAtivo();
+  }
+
+  private loadAtivo() {}
+
+  isCreateMode() {
+    return this.mode === 'create';
+  }
+  save() {
+    if (this.isCreateMode()) {
+      this.criarAplicacao();
+    }
+  }
+
+  criarAplicacao() {
+    //tratar erros
+  }
 }
